13c13c9a762c77c6f4a2f1cd31a82682fcd56f35
Richard Wang <richardwang301@gmail.com>
2019-10-18 17:37:32 +1100

    sharesight api

--- my-app/src/ToDoList.js
+++ my-app/src/ToDoList.js
@@ -11,6 +11,10 @@ class ToDoList extends React.Component {
     tasks: []
   };
 
+  // test = () => {
+  //   fetch("http://localhost:3000", { method: "POST" });
+  // };
+
   addTask = task => {
     // console.log("task added", this.state.tasks);
     this.setState({
@@ -24,19 +28,54 @@ class ToDoList extends React.Component {
     });
   };
 
-  // initiateConnection = () => {
-  //   const queryString = require("query-string");
+  getToken = () => {
+    const auth_code =
+      "6384fcda16f7a58b83b2b04304bb28a59bdd3a682dd975bd0e2a9f486310d183";
+    const redirect_URI = "urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob";
+    const google_URI = "https%3A%2F%2Fwww.google.com.au";
+    const client_secret =
+      "7bffa2c176b56e20a127c54dffeac2c9262f8376ff290bbd64c72b8814154e2f";
+    const client_id =
+      "294b7d9742ca01f342cf66147612ffc8f092ba18b56895583ef46d89cad964df";
+    let tokenURL = `https://edge-api.sharesight.com/oauth2/token?grant_type=authorization_code&code=${auth_code}&redirect_uri=${redirect_URI}&client_id=${client_id}&client_secret=${client_secret}`;
+    fetch(tokenURL, { method: "POST" })
+      .then(res => res.json())
+      .then(res => {
+        const auth_token = res.access_token;
+        const refresh_token = res.refresh_token;
+        console.log("got it", auth_token, refresh_token);
+        const singleSignIn =
+          "https://edge-api.sharesight.com/api/v2/single_sign_on.json";
+        fetch(singleSignIn, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${auth_token}`
+          }
+        })
+          .then(res => res.json())
+          .then(res => window.open(res.login_url));
+      });
+  };
 
-  //   const client_id =
-  //     "294b7d9742ca01f342cf66147612ffc8f092ba18b56895583ef46d89cad964df";
-  //   const redirect_URI = "urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob";
-  //   let url = `https://edge-api.sharesight.com/oauth2/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirect_URI}`;
-  //   // fetch(url).then(res => console.log(res));
-  //   window.open(url);
-  //   //now grab the code in the url
-  //   // const parsed = queryString.parse(window.location.search);
-  //   // console.log(parsed);
-  // };
+  initiateConnection = () => {
+    const queryString = require("query-string");
+    const client_id =
+      "294b7d9742ca01f342cf66147612ffc8f092ba18b56895583ef46d89cad964df";
+    const redirect_URI = "urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob";
+    const google_URI = "https%3A%2F%2Fwww.google.com.au";
+
+    let url = `https://edge-api.sharesight.com/oauth2/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirect_URI}`;
+    fetch(url).then(res => {
+      if (res.status == 200) {
+        window.open(url);
+        //retrieve auth_code
+      }
+    });
+    // window.open(url);
+    //now grab the code in the url
+    // const parsed = queryString.parse(window.location.search);
+    // console.log(parsed);
+  };
 
   render() {
     return (
@@ -50,7 +89,9 @@ class ToDoList extends React.Component {
         <ToDoForm addTask={this.addTask} />
         {this.individualTasks()}
         <br />
-        {/* <button onClick={this.initiateConnection}>Connect to Sharesight</button> */}
+        <button onClick={this.initiateConnection}>Connect to Sharesight</button>
+        <button onClick={this.getToken}>Get Token</button>
+        <button onClick={this.test}>Test</button>
       </div>
     );
   }
