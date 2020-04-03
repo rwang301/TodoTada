ff78d6122d3818f297ca44e90fa4b121613b8ac6
Richard Wang <richardwang301@gmail.com>
2020-04-03 22:36:51 +1100

    expressjs added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,12 +4,37 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import MenuAppBar from "./components/MenuAppBar";
 import DisplayContent from "./DisplayContent";
 
-function App() {
-  return (
-    <div className="App">
-      <DisplayContent />
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <DisplayContent />
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+  }
+  componentWillMount() {
+    this.callAPI();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <DisplayContent test={this.props.apiResponse} />
+        {console.log(this.state.apiResponse, "bitch you played!!!")}
+      </div>
+    );
+  }
 }
 
 export default App;
